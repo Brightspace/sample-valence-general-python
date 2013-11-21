211e481919aa9de2617c542560c8e71513a2de40
Viktor Haag <Viktor.Haag@Desire2Learn.com>
2013-11-21 16:25:51 -0500

    rewrote getting started sample to (a) strip it down in line with the other basic samples, and (b) track more the handling standards used for the example LTI Tool Provider in the docs

---
+++ Basic/conf_basic.py
@@ -0,0 +1,12 @@
+app_config = { 'app_id': 'G9nUpvbZQyiPrk3um2YAkQ',
+               'app_key': 'ybZu7fm_JKJTFwKEHfoZ7Q',
+               'host': 'localhost',
+               'port': '8080',
+               'scheme': 'HTTP',
+               'lms_host': 'valence.desire2learn.com',
+               'lms_port': '443',
+               'encrypt_requests': True,
+               'verify' : False,
+               'lms_ver': {'lp':'1.0','le':'1.0','ep':'2.0'},
+               'debug': True
+               }
---
+++ Basic/views/whoami.tpl
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head>
+     <title>Whoami results for {{first_name}} {{last_name}}</title>
+  </head>
+  <body>
+    Hello, {{first_name}}; your last name is {{last_name}}.
+  </body>
+</html>
